2bae5bb3b5e079770ff0ce9a32d101592ac43a99
ANTAQ <mymillions@ya.ru>
2025-07-05 20:10:46 +0300

    добавлена кастомная команда для загрузки тестовых данных, созданы фикстуры для моделей Category и Product

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # отображение в списке
+    search_fields = ('name',)      # поиск по name (можно добавить и description, если нужно)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # поля для отображения в списке
+    list_filter = ('category',)                         # фильтрация по категории
+    search_fields = ('name', 'description')             # поиск по name и description
